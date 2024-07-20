76e794fba29ff03f93e8acab607763b9902c5e6d
Anurag Tomar <atomar2000@gmail.com>
2024-07-20 15:57:31 +0530

    adding docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
-    mode: 'development',
     output: {
         filename: 'index.bundle.js',
         path: path.resolve(__dirname, 'dist'),
